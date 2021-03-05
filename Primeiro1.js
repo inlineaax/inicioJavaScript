720e17fc4a5eb660236e555642905b07b9ffc7f8
inlineaax <inl.aax@gmail.com>
2021-03-04 23:55:13 -0300

    Primeiro.JS

---
+++ Primeiro1.js
@@ -0,0 +1,13 @@
+//primeiro algoritimo
+
+var firstnumber;
+var secondnumber;
+var avg;
+
+console.log("Informe um número: ")
+firstnumber=3
+console.log("Informe outro número: ")
+secondnumber=6
+avg=(firstnumber+secondnumber)/2
+console.log(`A média de ${firstnumber}+${secondnumber} ${avg}`)
+
